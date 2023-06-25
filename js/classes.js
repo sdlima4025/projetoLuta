dad7be46b25130a7c866be234ec5c12961dd58da
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-25 17:13:36 -0300

    criado base do projeto fight

---
+++ js/classes.js
@@ -0,0 +1,69 @@
+class Character {
+
+    _life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+    get life() {
+        return this._life;
+    }
+    set life(newLife) {
+        this._life = newLife < 0 ? 0 : newLife;
+    }
+}
+
+
+
+class Knight extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life;
+    }
+}
+
+class Sorcerer extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 80;
+        this.attack = 16;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class LittleMonster extends Character {
+    constructor() {
+        super('Little Monster');
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+class BigMonster extends Character {
+    constructor() {
+        super('Big Monster');
+        this.life = 120;
+        this.attack = 16;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
+
+class Stage {
+    constructor(fighter1,fighter2,fighter1El,fighter1El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = this.fighter2El;
+    }
+    start()
+}
+
---
+++ js/script.js
@@ -0,0 +1,3 @@
+let char = new Knight();
+let monster = new LittleMonster();
+
