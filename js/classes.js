0c1c563fcf4105253d1e0bc618c7b3aa261eb699
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-27 17:35:51 -0300

    criado update e function doAttack

--- js/classes.js
+++ js/classes.js
@@ -57,13 +57,36 @@ class BigMonster extends Character {
     }
 }
 
+
 class Stage {
-    constructor(fighter1,fighter2,fighter1El,fighter1El) {
-        this.fighter1 = fighter1;
-        this.fighter2 = fighter2;
-        this.fighter1El = fighter1El;
-        this.fighter2El = this.fighter2El;
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+       this.fighter1 = fighter1;
+       this.fighter2 = fighter2;
+       this.fighter1El = fighter1El;
+       this.fighter2El = fighter2El;
     }
-    start()
+    start() {
+        this.update();
+        this.fighter1El.querySelector('.attackButon').addEventListener('click',() => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButon').addEventListener('click',() => this.doAttack(this.fighter2, this.fighter1));
+    }
+
+    update() {
+         // figther 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP;`; 
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width  = `${f1Pct}%`;
+
+        // figther 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP;`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+
+    }
+    doAttack(attacking, attacked) {
+        console.log(`${attacking.name} está atacando ${attacked.name}`);
+
+        this.update();
+    }
 }
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-let char = new Knight();
+let char = new Knight('Sérgio');
 let monster = new LittleMonster();
 
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
+
+stage.start();
